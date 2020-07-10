406d5d1fa681eee0297914f4e74378ade26b9cb5
ohad <ohadas88@gmail.com>
2020-07-10 17:34:19 +0300

    only likes feature missing

--- app.js
+++ app.js
@@ -2,3 +2,27 @@ const formBtn = $("#BTN");
 const cityInput = $("#cityInput");
 const imgInput = $("#imgInput");
 const cardsContainer = $("#cardsContainer");
+
+formBtn.on("click", function () {
+  //   cardsContainer.append
+  _createCard(cityInput.val(), imgInput.val()).appendTo($("#DrawCards"));
+
+  function _createCard(cityInput, imgInput) {
+    const card = $(
+      `<div class="singleCard"><img src= ${imgInput} style="width:100%"><div class="container"><h4><b>${cityInput}</b></h4><button><img id="likeBtn" src="./Images/unnamed.png"</button> <p>Likes:<span id="likesNum">0</p></div></div>`
+    );
+
+    return card;
+
+    function _increaseLikes() {}
+  }
+});
+{
+  /* <div class="card">
+  <img src="img_avatar.png" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>John Doe</b></h4>
+    <p>Architect & Engineer</p>
+  </div>
+</div> */
+}
