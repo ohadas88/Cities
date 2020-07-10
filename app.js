4d692d051cf7b89e0c6d4bb82e623233bcfdc213
ohad <ohadas88@gmail.com>
2020-07-10 18:29:02 +0300

    likes feat

--- app.js
+++ app.js
@@ -7,14 +7,20 @@ formBtn.on("click", function () {
   //   cardsContainer.append
   _createCard(cityInput.val(), imgInput.val()).appendTo($("#DrawCards"));
 
+  $("#likeBtn").on("click", function () {
+    setTimeout(() => {
+      const currentNumberOfLikes = Number($("#likesNum").text());
+      const newNumberOfLikes = currentNumberOfLikes + 1;
+      Number($("#likesNum").text(newNumberOfLikes));
+    }, 2000);
+  });
+
   function _createCard(cityInput, imgInput) {
     const card = $(
-      `<div class="singleCard"><img src= ${imgInput} style="width:100%"><div class="container"><h4><b>${cityInput}</b></h4><button><img id="likeBtn" src="./Images/unnamed.png"</button> <p>Likes:<span id="likesNum">0</p></div></div>`
+      `<div class="singleCard"><img src= ${imgInput} style="width:100%"><div class="container"><h4><b>${cityInput}</b></h4><button><img id="likeBtn" src="./Images/unnamed.png"</button> <p>Likes:<span id="likesNum">0</span></p></div></div>`
     );
 
     return card;
-
-    function _increaseLikes() {}
   }
 });
 {
